c3452ae4f6187e243dd483757754bf2bfd3eaf17
Cedric Pinson <cedric.pinson@plopbyte.com>
2011-06-23 04:12:52 +0200

    tune and try effect

--- js/particles.js
+++ js/particles.js
@@ -39,21 +39,30 @@ var initParticles = function() {
             "uniform sampler2D PosY;",
             "uniform sampler2D PosZ;",
             "uniform sampler2D DistanceMap;",
+            "uniform int freeze;",
+            "uniform float weightDistanceMap;",
+            "uniform float weightVelocityField;",
             "float life;",
             "float distance;",
+            "float material;",
             "float unpack(vec4 vec) {",
             "  return vec[0] * 255.0 / 256.0 + vec[1] * 255.0 / (256.0 * 256.0) + vec[2] * 255.0 / (256.0 * 256.0 * 256.0);",
             "}",
             "vec4 pack(float value) {",
             "  return vec4(floor(value * 256.0)/255.0, floor(fract(value * 256.0) * 256.0)/255.0 , floor(fract(value * 65536.0) * 256.0)/255.0, 0.0);",
             "}",
-            "//vec3 getSpawnPosition() {",
-            "",
-            "//}",
+            "vec3 getSpawnPosition(float offset) {",
+            "   vec3 center = vec3(0.5, 0.70, 0.5);",
+            "   vec2 size = vec2(0.7, 0.4);",
+            "   vec3 corner = center - vec3(size.x/2.0, 0.0, size.y/2.0);",
+            "   vec3 pos = vec3(size.x* FragTexCoord0.x, -offset + 0.1 + 0.1*cos(4.0*FragTexCoord0.x), size.y*FragTexCoord0.y + 0.05*cos(4.0*FragTexCoord0.x))+corner;",
+            "   material = 0.0;",
+            "   return pos;",
+            "}",
             "vec3 getVelocityField(vec3 pos) {",
-            "   float vx = 0.0+cos(0.5+2.0*(pos.x));",
-            "   float vy = cos(4.0*(pos.y+0.5)) + sin(4.0*pos.x);",
-            "   float vz = cos(pos.z*2.0);",
+            "   float vx = 0.0+cos(0.5+2.0*(pos.x*pos.x*time));",
+            "   float vy = cos(4.0*(pos.y*time+0.5)) + sin(4.0*pos.x*time*time);",
+            "   float vz = cos(pos.z*2.0*time);",
             "   vec3 vel = vec3( vx, vy, vz);",
             "   return normalize(vel);",
             "}",
@@ -68,56 +77,63 @@ var initParticles = function() {
             "   vec4 p1 = texture2D( PosY, vec2(FragTexCoord0.x, FragTexCoord0.y));",
             "   vec4 p2 = texture2D( PosZ, vec2(FragTexCoord0.x, FragTexCoord0.y));",
             "   life = p0[3];",
+            "   material = p2[3];",
             "   return vec3(unpack(p0), unpack(p1), unpack(p2));",
             "}",
             "float getDistance(vec3 pos) {",
-            "   return texture2D( DistanceMap, vec2(pos.x, pos.z)).b;",
+            "   float d = texture2D( DistanceMap, vec2(pos.x, pos.z)).b;",
+            "   return d;",
+            "   float d2 = 2.0*abs(0.5-pos.y);",
+            "   return max(d2,d);",
             "}",
             "vec3 getDirection(vec3 pos) {",
             "   vec4 d = texture2D( DistanceMap, vec2(pos.x, pos.z));",
             "   vec2 grad = d.rg;",
-            "   vec3 dir = vec3(grad[0]-0.5, 0.5-pos.y, grad[1]-0.5);",
-            "   dir = normalize(dir) * (1.0 - d.b);",
+            "   vec3 dir = vec3(0.5-grad[0], 0.125*(0.5-pos.y), 0.5-grad[1]);",
+            "   //dir = normalize(dir) * (1.0 - d.b);",
+            "   dir = normalize(dir);",
             "   return dir;",
             "}",
             "vec3 verlet(vec3 prevPosition, vec3 currentPosition, float dt) {",
             "   if (life <= 3.0/255.0 && life >= 1.0/255.0) {",
+            "      return getSpawnPosition(0.0);",
             "      float amp = 0.005;",
             "      return vec3(0.5+sin(FragTexCoord0.x*2.0+time)*amp, 0.5+sin(FragTexCoord0.y*2.0+time)*amp, 0.5 + cos(FragTexCoord0.y*2.0+time)*amp);",
             "   } else if (life < 1.0/255.0) {",
             "      life = sin(FragTexCoord0.y*time*0.2) * 0.25 + cos(FragTexCoord0.x*time*1.5) * 0.25 + sin(time) * 0.25 + 0.74;",
-            "      //return currentPosition + (currentPosition - prevPosition);",
-            "      return vec3(0.5, 0.5, 0.5);",
+            "      return getSpawnPosition(0.005);",
             "   }",
-            "   vec3 acceleration = vec3(0.0, 0.0, (-9.81 + 9.5));",
-            "   //vec3 targetVec = (vec3(sin(time)*0.25 + 0.5, 0.5 + cos(time*0.2)*0.25, 0.75) - currentPosition)*0.2;",
-            "   vec3 targetVec = (vec3(sin(time)*0.25 + 0.5, 0.5 + cos(time*0.2)*0.25, 0.75) - currentPosition)*1.0;",
-            "   targetVec += getVelocityField(currentPosition) * 0.2;",
-            "   targetVec = getDirection(currentPosition)*0.4;",
+            "   vec3 acceleration = vec3(0.0, 0.0, 0.0*(-9.81 + 9.5));",
+            "   vec3 targetVec = vec3(0.0,0.0,0.0);",
+            "   targetVec = getVelocityField(currentPosition)*weightVelocityField*0.5;",
+            "   targetVec += getDirection(currentPosition)*weightDistanceMap*0.4;",
             "   ",
             "   vec3 velocity = (currentPosition-prevPosition);",
             "   float wind = 1.0;",
-            "   acceleration += targetVec;",
-            "   wind = 1.0;",
+            "   acceleration += targetVec * 0.1;",
             "   ",
-            "   acceleration *= 0.5;",
-            "   distance = getDistance(currentPosition);",
-            "   if ( distance > 0.2+0.5) {",
-            "      acceleration *= 0.5;",
+            "   //acceleration *= 0.5;",
+            "   distance = getDistance(currentPosition)*weightDistanceMap;",
+            "   if ( distance > 0.3) {",
+            "      //acceleration *= 0.5;",
             "      //velocity *= 0.0;",
-            "      wind = 2.5;",
+            "      //wind = 2.5;",
+            "      //velocity *= 0.98;",
+            "      //acceleration *= 0.01;",
+            "      //wind = 0.0;",
+            "      if (freeze == 1) {",
+            "         material = 1.0;",
+            "      }",
             "   }",
             "      ",
             "   vec3 dir = normalize(velocity);",
-            "   if (length(velocity) > 0.001) {",
-            "     acceleration += -dir * 0.35 * wind;",
+            "   float l = length(velocity);",
+            "   float maxSpeed = 0.005;",
+            "   if (l > 0.001) {",
+            "      acceleration += -dir * 0.35 * wind;",
+            "      velocity = dir * min(maxSpeed, l); //cap velocity",
             "   }",
-            "   vec3 next;",
-            "   if (true || time > 1.0) {",
-            "   next = (currentPosition + velocity + acceleration * (dt * dt));",
-            "   } else {",
-            "   next = vec3(sin(time) + FragTexCoord0.x, sin(time), cos(time) + FragTexCoord0.y);",
-            "   }",
+            "   vec3 next = (currentPosition + velocity + acceleration * (dt * dt));",
             "   return next;",
             "}",
             "",
@@ -139,6 +155,7 @@ var initParticles = function() {
             "      value[3] = distance;",
             "   } else if (bits == 2){",
             "      value = z;",
+            "      value[3] = material;",
             "   }",
             "   gl_FragColor = value;",
             "}",
@@ -189,6 +206,9 @@ var initParticles = function() {
 
 
     var uniformTime = osg.Uniform.createFloat1(0.0,'time');
+    var freeze = osg.Uniform.createInt1(0,'freeze');
+    var weightVelocityField = osg.Uniform.createFloat1(1,'weightVelocityField');
+    var weightDistanceMap = osg.Uniform.createFloat1(1,'weightDistanceMap');
 
     var Physics = function(cameras, textures) {
         this.cameras = cameras;
@@ -270,6 +290,9 @@ var initParticles = function() {
             stateset.addUniform(distanceMap);
 
             stateset.addUniform(uniformTime);
+            stateset.addUniform(freeze);
+            stateset.addUniform(weightVelocityField);
+            stateset.addUniform(weightDistanceMap);
             
             var idx;
             idx = (index + 1)%3;
@@ -351,6 +374,7 @@ var initParticles = function() {
             "varying vec4 color;",
             "float life;",
             "float distance;",
+            "float material;",
             "float unpack(vec4 vec) {",
             "  return vec[0] * 255.0 / 256.0 + vec[1] * 255.0 / (256.0 * 256.0) + vec[2] * 255.0 / (256.0 * 256.0 * 256.0);",
             "}",
@@ -365,11 +389,13 @@ var initParticles = function() {
             "  vec2 uv = vec2(Vertex.x, Vertex.y);",
             "  vec4 xvec = texture2D( X, uv);",
             "  vec4 yvec = texture2D( Y, uv);",
+            "  vec4 zvec = texture2D( Z, uv);",
             "  life = xvec[3];",
             "  distance = yvec[3];",
+            "  material = zvec[3];",
             "  float x = unpack(xvec);",
             "  float y = unpack(yvec);",
-            "  float z = unpack(texture2D( Z, uv));",
+            "  float z = unpack(zvec);",
             "  vec4 p = vec4(x,y,z,0);",
             "  vec4 v;",
             "  //v[0] = (p[0] - 0.5) * 1.0;",
@@ -386,12 +412,13 @@ var initParticles = function() {
             "     distFromEdge = 1.0 - 2.0 * abs( distance-0.5);",
             "  }",
             "  color = vec4(x * alpha , y * alpha, z * alpha, alpha * distFromEdge);",
-            "  if (distance > 0.5 && distance < .8 ) {",
+            "  if (material > 0.5) {",
+            "  //if (distance > 0.5 && distance < .8 ) {",
             "     float b = 0.0 * (1.0-distance);",
             "     color = vec4(b, b, b, 1.0 * alpha);",
             "  }",
             "  gl_Position = ProjectionMatrix * ModelViewMatrix * v;",
-            "  gl_PointSize = 1.0;",
+            "  gl_PointSize = 2.0;",
             "}",
             ""
         ].join('\n');
@@ -436,9 +463,13 @@ var initParticles = function() {
     };
     UpdateCallback.prototype = {
         update: function(node, nv) {
+            var t = nv.getFrameStamp().getSimulationTime();
+            weightVelocityField.set([0.5 + 0.5*Math.cos(t*0.2)]);
+            weightDistanceMap.set([1.0 + 0.0* (0.5 + 0.5*Math.cos(t*0.666666))]);
+
             this.physics.switchBuffer();
             this.render.setDisplayTexture( this.physics.getDisplayTexture() );
-            uniformTime.set([nv.getFrameStamp().getSimulationTime()]);
+            uniformTime.set([t]);
             node.traverse(nv);
         }
     };
@@ -450,6 +481,19 @@ var initParticles = function() {
 
     root.addChild(physics.root);
     root.addChild(render.root);
+
+
+    document.addEventListener(
+        "keydown", 
+        function(event) {
+            osg.log("freeze");
+            freeze.set([1]);
+            window.setTimeout(function() {
+                freeze.set([0]);
+                osg.log("unfreeze");
+            },1000);
+        }, 
+        false);
 
     return root;
 
